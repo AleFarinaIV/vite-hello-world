ebb09a10e7bbe54c00b21ff618c511eec7853e20
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-07-26 16:02:56 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import AppTitle from "./components/AppTitle"
+export default {
+  components: {
+    AppTitle
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
